a9eac3dff4fee7c7c58a0ae436ac0aa59586101e
rohithsarji <rohithssarji@gmail.com>
2025-09-24 23:27:05 +0530

    AMXDRCP-100 architecture

---
+++ app.py/config.py
@@ -0,0 +1,8 @@
+import os
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL", "sqlite:///data.db"
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret")
